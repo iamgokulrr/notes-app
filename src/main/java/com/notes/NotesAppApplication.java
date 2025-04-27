d74b1a577ef1fa1f07ad78de2c1699a005ad9f03
iamgokulrr <iamgokulrr@gmail.com>
2025-04-27 19:10:25 +0530

    second commit

--- src/main/java/com/notes/NotesAppApplication.java
+++ src/main/java/com/notes/NotesAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotesAppApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NotesAppApplication.class, args);
 	}
